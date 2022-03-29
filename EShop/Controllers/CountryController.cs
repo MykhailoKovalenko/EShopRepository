f54234f13e9d4cbc58423dc5ab2e47923e816dbd
MykhailoKovalenko <m.kovalenko2791@gmail.com>
2022-03-29 23:19:59 +0300

    Category impl (finalization)

--- EShop/Controllers/CountryController.cs
+++ EShop/Controllers/CountryController.cs
@@ -30,7 +30,74 @@ namespace EShop.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Country country)
         {
+            if(!ModelState.IsValid)
+            {
+                return View(country);
+            }
+
             _context.Countries.Add(country);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var country = _context.Countries.Find(id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            return View(country);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Country country)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(country);
+            }
+            _context.Countries.Update(country);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var country = _context.Countries.Find(id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            return View(country);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var country = _context.Countries.Find(id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            _context.Countries.Remove(country);
             _context.SaveChanges();
 
             return RedirectToAction("Index");
