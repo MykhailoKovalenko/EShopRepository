5a2c41838e78ef3377c9c070ab6ad02a361f302f
MykhailoKovalenko <m.kovalenko2791@gmail.com>
2022-03-30 00:34:37 +0300

    Product model and Product Index page

---
+++ EShop/Controllers/ProductController.cs
@@ -0,0 +1,114 @@
+﻿using EShop.Data;
+using EShop.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EShop.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDBContext _context;
+
+        public ProductController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Product> products = _context.Products;
+
+            foreach(var product in products)
+            {
+                product.Category = _context.Categories.FirstOrDefault(i => i.Id == product.CategoryId);
+            }
+
+            return View(products);
+        }
+
+    //    public IActionResult Create()
+    //    {
+    //        return View();
+    //    }
+
+    //    [HttpPost]
+    //    [ValidateAntiForgeryToken]
+    //    public IActionResult Create(Category category)
+    //    {
+    //        if (!ModelState.IsValid)
+    //        {
+    //            return View(category);
+    //        }
+    //        _context.Categories.Add(category);
+    //        _context.SaveChanges();
+
+    //        return RedirectToAction("Index");
+    //    }
+
+    //    public IActionResult Edit(int? id)
+    //    {
+    //        if (id == null || id == 0)
+    //        {
+    //            return NotFound();
+    //        }
+
+    //        var category = _context.Categories.Find(id);
+    //        if (category == null)
+    //        {
+    //            return NotFound();
+    //        }
+
+    //        return View(category);
+    //    }
+
+    //    [HttpPost]
+    //    [ValidateAntiForgeryToken]
+    //    public IActionResult Edit(Category category)
+    //    {
+    //        if (!ModelState.IsValid)
+    //        {
+    //            return View(category);
+    //        }
+    //        _context.Categories.Update(category);
+    //        _context.SaveChanges();
+
+    //        return RedirectToAction("Index");
+    //    }
+
+    //    public IActionResult Delete(int? id)
+    //    {
+    //        if (id == null || id == 0)
+    //        {
+    //            return NotFound();
+    //        }
+
+    //        var category = _context.Categories.Find(id);
+    //        if (category == null)
+    //        {
+    //            return NotFound();
+    //        }
+
+    //        return View(category);
+    //    }
+
+    //    [HttpPost]
+    //    [ValidateAntiForgeryToken]
+    //    public IActionResult DeletePost(int? id)
+    //    {
+    //        var category = _context.Categories.Find(id);
+    //        if (category == null)
+    //        {
+    //            return NotFound();
+    //        }
+
+    //        _context.Categories.Remove(category);
+    //        _context.SaveChanges();
+
+    //        return RedirectToAction("Index");
+    //    }
+    //}
+}
+}
--- EShop/Migrations/20220329210158_AddProductModel2.Designer.cs
+++ EShop/Migrations/20220329210158_AddProductModel2.Designer.cs
@@ -3,14 +3,16 @@ using EShop.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EShop.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    partial class ApplicationDBContextModelSnapshot : ModelSnapshot
+    [Migration("20220329210158_AddProductModel2")]
+    partial class AddProductModel2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
